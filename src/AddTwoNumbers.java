70665e3d8e12f6958a3abda46891610649e00b78
MuazS <muazshaikh11@gmail.com>
2025-02-06 23:30:48 -0500

    Updated for next time

--- src/AddTwoNumbers.java
+++ src/AddTwoNumbers.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+class AddTwoNumbers {
 
-public class AddTwoNumbers {
+    /*
+    You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+    You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+    Example 2:
+    Input: l1 = [0], l2 = [0]
+    Output: [0]
+
+    Example 3:
+    Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+    Output: [8,9,9,9,0,0,0,1]
+
+
+     */
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        return l1;
+    }
 }
